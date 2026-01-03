2dda41e0b7278bab7eeec5655a86a0bdf2b5976d
itzmosaa <kaigenshocks@gmail.com>
2026-01-03 18:11:28 -0500

    Create AccountSystem.lua

---
+++ AccountSystem.lua
@@ -0,0 +1,16 @@
+return {
+    Accounts = {
+        {
+            Username = "mosa",
+            Password = "sigmarizz",
+            HWID = "ANY",
+            IsActive = true
+        },
+        {
+            Username = "karen",
+            Password = "bedwars12345",
+            HWID = "ANY",
+            IsActive = true
+        }
+    }
+}
